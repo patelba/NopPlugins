fe332e2b122bddf1d31395b5d401a1a15659012d
patelba <bhupendra.patel@yahoo.com>
2015-07-13 22:18:11 +0100

    Initial Set up

---
+++ BeePee/BeePee.Plugin.Admin.Migration/Controllers/MigrationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Services.Configuration;
+using Nop.Web.Framework.Controllers;
+
+namespace BeePee.Plugin.Admin.Migration.Controllers
+{
+    public class MigrationController : BasePluginController 
+    {
+        private readonly ISettingService _settingService;
+
+        public MigrationController(ISettingService settingService)
+        {
+            _settingService = settingService;
+        }
+
+
+    }
+}
---
+++ BeePee/BeePee.Plugin.Messaging.Alerts/ReadMe.txt
@@ -0,0 +1,4 @@
+﻿Uses the workflow service to send notification to required party
+Uses mailChimp for Email Notification
+Uses twilio for text Notification
+Uses Slack API for Instant Messenger notification
